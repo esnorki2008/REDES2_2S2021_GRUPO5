7a3a1add1fb91f20be4942b893aba986715a578f
enk <enk>
2021-11-05 00:29:55 -0600

    pag

--- Practica2/backend/src/config/configuration.ts
+++
@@ -1,5 +0,0 @@
-export default () => ({
-    server_intern: process.env.SERVER,
-    port: 3001 || process.env.PORT
-  });
-  
--- Practica2/frontend/src/app/interface/asistencia.ts
+++
@@ -1,9 +0,0 @@
-export interface Asistencia {
-    carnet: String;
-    nombreEstudiante:String;
-    nombreEvento:String;
-    idEvento:number;
-    imagen:String;
-}
-
-
